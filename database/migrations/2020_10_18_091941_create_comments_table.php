e7a63a42319947bd2e758ffc393f015682240e9b
Osman Forhad <osmanforhad93@gmail.com>
2020-10-18 15:51:02 +0600

    creating model and migration

--- database/migrations/2020_10_18_091941_create_comments_table.php
+++ database/migrations/2020_10_18_091941_create_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->integer('is_admin')->unsigned()->default(0);
-            $table->rememberToken();
+            $table->integer('ticket_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->text('comment');
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('comments');
     }
 }
