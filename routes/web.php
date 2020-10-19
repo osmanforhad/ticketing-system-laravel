87aa2758099c879e9767812ab33669e5ad124c5b
Osman Forhad <osmanforhad93@gmail.com>
2020-10-19 15:10:24 +0600

    working with applications route

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,22 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', 'HomeController@index');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('new-ticket', 'TicketsController@create');
+Route::post('new-ticket', 'TicketsController@store');
+
+Route::get('my_tickets', 'TicketsController@userTickets');
+Route::get('tickets/{ticket_id}', 'TIcketsController@show');
+
+Route::post('comment', 'CommentsController@postComment');
+
+Route::group(['prefix' => 'admin', 'middleware' => 'admin'], function () {
+    Route::get('tickets', 'TicketsController@index');
+    Route::post('close_ticket/{ticket_id}', 'TicketsController@close');
+});
