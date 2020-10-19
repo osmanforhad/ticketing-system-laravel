2ccade6cf5c717e44a0de8ce64e3dcd99c369d08
Osman Forhad <osmanforhad93@gmail.com>
2020-10-19 12:18:03 +0600

    settipup Authentication

---
+++ app/Http/Controllers/CommentsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CommentsController extends Controller
+{
+    public function postComment(Request $request)
+    {
+
+    } //end of the postComment method
+
+} //end of the class
---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!Auth::check() || (Auth::check() && Auth::user()->is_admin !== 1)) {
+            return redirect('home'); // if the user not login or he is login but not is_admin then we redirect him  to the landing page else we proceed
+        }
+        return $next($request);
+
+    } //end of the handle method
+
+} //end of the class
