a242fa9dc91f41cd12e02e70bf36a639bbcc7f88
Osman Forhad <osmanforhad93@gmail.com>
2020-10-20 13:34:24 +0600

    working with Maielr class

---
+++ app/Mailers/AppMailer.php
@@ -0,0 +1,65 @@
+<?php
+namespace App\Mailers;
+
+use App\Ticket;
+use Illuminate\Contracts\Mail\Mailer;
+
+class AppMailer
+{
+    protected $mailer;
+    protected $fromAddress = 'support@supportticket.dev';
+    protected $fromName = 'Support Ticket';
+    protected $to;
+    protected $subject;
+    protected $view;
+    protected $data = [];
+
+    /**
+     *AppMailer constructor
+     *param $mailer
+     */
+    public function __construct()
+    {
+        $this->mailer = $mailer;
+    } //end of the constructor method
+
+    /**
+     * method for sending ticket info
+     */
+    public function sendTicketInformation($user, Ticket $ticket)
+    {
+        $this->to = $user->email;
+
+        $this->subject = "[Ticket ID: $ticket->ticket_id] $ticket->title";
+
+        $this->view = 'emails.ticket_info';
+
+        $this->data = compact('user', 'ticket');
+
+        return $this->deliver();
+    } //end of the sendTicketInformation method
+
+    /**
+     * method for comment section
+     */
+    public function sendTicketComments($ticketOwner, $user, Ticket $ticket, $comment)
+    {
+
+    } //end of the sendTicketsComment
+
+    /**
+     * method for ticketStatus Notification
+     */
+    public function sendTicketStatusNotification($ticketOwner, Ticket $ticket)
+    {
+
+    } //end of the sendTicketStatusNotification method
+
+    public function deliver()
+    {
+        $this->mailer->send($this->view, $this->data, function ($message) {
+            $message->form($this->fromAddress, $this->fromName)->to($this->to)->subject($this->subject);
+        });
+    } //end of the deliver method
+
+} //end of the class
